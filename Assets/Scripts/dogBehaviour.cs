7b1a7b602859701b8b28ead0e44641b0d1416cb0
Zac <brickman1444@gmail.com>
2014-12-06 14:10:38 -0600

    skeleton of dog movement.

---
+++ Assets/Scripts/dogBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dogBehaviour : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 toAvatar = avatarMovement.shittyInstance.transform.position - transform.position;
+        toAvatar.Normalize();
+
+        Vector3 velocity = toAvatar * speed * Time.deltaTime;
+
+        rigidbody2D.velocity = velocity;
+	}
+}
