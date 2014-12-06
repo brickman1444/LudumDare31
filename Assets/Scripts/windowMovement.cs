584af11a31745a27cdb2f6a591152efd3abbb711
Zac <brickman1444@gmail.com>
2014-12-05 21:58:14 -0600

    new scene and moving window within confines.

---
+++ Assets/Scripts/windowMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class windowMovement : MonoBehaviour {
+
+    public float speed;
+    public GameObject maxObject;
+    public GameObject minObject;
+
+    Vector3 maxPos;
+    Vector3 minPos;
+
+	// Use this for initialization
+	void Start () {
+        maxPos = maxObject.transform.position;
+        minPos = minObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 velocity = new Vector3();
+
+        velocity += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
+        velocity += Vector3.up * Time.deltaTime * speed * Input.GetAxis("Vertical");
+
+        Vector3 newPos = transform.position + velocity;
+
+        if (newPos.x > maxPos.x)
+        {
+            newPos.x = maxPos.x;
+        }
+        else if (newPos.x < minPos.x)
+        {
+            newPos.x = minPos.x;
+        }
+
+        if (newPos.y > maxPos.y)
+        {
+            newPos.y = maxPos.y;
+        }
+        else if (newPos.y < minPos.y)
+        {
+            newPos.y = minPos.y;
+        }
+
+        transform.position = newPos;
+	}
+}
