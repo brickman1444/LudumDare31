3d584fdeba52be1d8af1b0a63c6e40bd39a31f5c
Zac <brickman1444@gmail.com>
2014-12-05 22:24:47 -0600

    Add avatar movement

---
+++ Assets/Scripts/avatarMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class avatarMovement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 velocity = new Vector3();
+
+        velocity += Vector3.right * Time.deltaTime * speed * Input.GetAxis("AD");
+        velocity += Vector3.up * Time.deltaTime * speed * Input.GetAxis("WS");
+
+        transform.position += velocity;
+	}
+}
