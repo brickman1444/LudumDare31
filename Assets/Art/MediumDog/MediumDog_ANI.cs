ac725e75cf7dea11c5fb904efbc667c567141c46
SHENSON18 <shenson8@yahoo.com>
2014-12-06 21:41:04 -0600

    Added sprite animation for medium_dog, large_dog, and mailman.  Created folders for each character

---
+++ Assets/Art/MediumDog/MediumDog_ANI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MediumDog_ANI : MonoBehaviour {
+	Animator mediumdog;
+	// Use this for initialization
+	void Start () {
+		mediumdog = GetComponent<Animator>();
+		mediumdog.SetInteger("MEDIUM_Dog",0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.E))
+		{
+			mediumdog.SetInteger("MEDIUM_Dog",0);  //resets to idle
+		}
+		if (Input.GetKey (KeyCode.V))
+		{
+			mediumdog.SetInteger("MEDIUM_Dog",6);  //resets to idle
+		}
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			mediumdog.SetInteger("MEDIUM_Dog",4);  
+		}
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			mediumdog.SetInteger("MEDIUM_Dog",1);
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			mediumdog.SetInteger("MEDIUM_Dog",2);
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			mediumdog.SetInteger("MEDIUM_Dog",3);
+		}
+
+		if (Input.GetKey (KeyCode.R)) 
+		{
+			mediumdog.SetInteger("MEDIUM_Dog",5);
+		}
+	}
+}
