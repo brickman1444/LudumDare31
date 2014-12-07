1caa52f9a652f0e09c599e8df05bc1184582a986
Zac <brickman1444@gmail.com>
2014-12-07 11:52:09 -0600

    Dog dying animation.

--- Assets/Scripts/dogBehaviour.cs
+++ Assets/Scripts/dogBehaviour.cs
@@ -7,60 +7,78 @@ public class dogBehaviour : MonoBehaviour {
     public float biteTime;
     public float bitePushBack;
     public string animationKey;
+    public AnimationClip deathAnimation;
 
     private float functionalSpeed;
     private Animator dogAnimator;
     private int lastAnimationSignal = -1;
+
+    private DogState currState;
+
+    enum DogState
+    {
+        Chase,
+        Die
+    }
 
 	// Use this for initialization
 	void Start () {
         functionalSpeed = speed;
         dogAnimator = GetComponent<Animator>();
         dogAnimator.SetInteger(animationKey, 0);
+        currState = DogState.Chase;
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
 
-        Vector3 toAvatar = avatarMovement.shittyInstance.transform.position - transform.position;
-        toAvatar.Normalize();
-
-        Vector3 velocity = toAvatar * functionalSpeed * Time.fixedDeltaTime;
-
-        int animationSignal = -1;
-
-        // find major axis of velocity
-        if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (currState)
         {
-            if (velocity.x > 0)
-            {
-                animationSignal = 1;
-            }
-            else
-            {
-                animationSignal = 3;
-            }
-        }
-        else if (Mathf.Abs(velocity.y) > Mathf.Abs(velocity.x))
-        {
-            if (velocity.y > 0)
-            {
-                animationSignal = 4;
-            }
-            else
-            {
-                animationSignal = 2;
-            }
-        }
+            case DogState.Chase:
+                Vector3 toAvatar = avatarMovement.shittyInstance.transform.position - transform.position;
+                toAvatar.Normalize();
 
-        if (animationSignal != -1 && animationSignal != lastAnimationSignal)
-        {
-            dogAnimator.SetInteger(animationKey, animationSignal);
+                Vector3 velocity = toAvatar * functionalSpeed * Time.fixedDeltaTime;
+
+                int animationSignal = -1;
+
+                // find major axis of velocity
+                if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
+                {
+                    if (velocity.x > 0)
+                    {
+                        animationSignal = 1;
+                    }
+                    else
+                    {
+                        animationSignal = 3;
+                    }
+                }
+                else if (Mathf.Abs(velocity.y) > Mathf.Abs(velocity.x))
+                {
+                    if (velocity.y > 0)
+                    {
+                        animationSignal = 4;
+                    }
+                    else
+                    {
+                        animationSignal = 2;
+                    }
+                }
+
+                if (animationSignal != -1 && animationSignal != lastAnimationSignal)
+                {
+                    dogAnimator.SetInteger(animationKey, animationSignal);
+                }
+
+                rigidbody2D.velocity = velocity;
+
+                break;
+            case DogState.Die: // Do nothing while animation plays
+                rigidbody2D.velocity = Vector2.zero;
+                break;
         }
-
-
-        rigidbody2D.velocity = velocity;
-	}
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -73,7 +91,6 @@ public class dogBehaviour : MonoBehaviour {
                 break;
             case "Player":
                 Bite();
-				Lives.LIVES -=1; //subtracts lives
                 break;
         }
     }
@@ -93,19 +110,36 @@ public class dogBehaviour : MonoBehaviour {
     void Die()
     {
         //Debug.Log("Die " + gameObject.name);
+        currState = DogState.Die;
+
+        dogAnimator.SetInteger(animationKey, 6);
+
+        Invoke("KillGameObject", deathAnimation.length);
+    }
+
+    void KillGameObject()
+    {
         Destroy(gameObject);
     }
 
     void Bite()
     {
-        //Debug.Log("Bite " + gameObject.name);
-        Vector3 avatarPos = avatarMovement.shittyInstance.transform.position;
-        Vector3 dirAwayFromAvatar = transform.position - avatarPos;
+        if (currState != DogState.Die)
+        {
+            //Debug.Log("Bite " + gameObject.name);
 
-        rigidbody2D.AddForce(dirAwayFromAvatar * bitePushBack);
+            dogAnimator.SetInteger(animationKey, 5);
 
-        functionalSpeed = 0;
-        Invoke("ResetFunctionalSpeed", biteTime);
+            Lives.LIVES -= 1; //subtracts lives
+
+            Vector3 avatarPos = avatarMovement.shittyInstance.transform.position;
+            Vector3 dirAwayFromAvatar = transform.position - avatarPos;
+
+            rigidbody2D.AddForce(dirAwayFromAvatar * bitePushBack);
+
+            functionalSpeed = 0;
+            Invoke("ResetFunctionalSpeed", biteTime);
+        }
     }
 
     void ResetFunctionalSpeed()
